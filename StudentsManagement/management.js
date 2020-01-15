8df1223c111554b80e9eec273144e12af8e0f9dc
anhlt <anhlt>
2020-01-15 21:53:01 +0700

    Add layout for database management file

--- StudentsManagement/management.js
+++ StudentsManagement/management.js
@@ -1,3 +1,28 @@
 const express = require('express');
 const mysql = require('mysql');
 
+const router = express.Router();
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "guest",
+    password: "guest",
+    database: "students_info"
+});
+
+router.get('/', (req, res, next) => {
+    // TODO: show all students
+    res.end();
+});
+
+router.get('/:id', (req, res, next) => {
+    // TODO: show student with the given id
+    res.end();
+});
+
+router.post('/', (req, res, next) => {
+    // TODO: create new student / update student
+    res.end();
+});
+
+// TODO: delete student
