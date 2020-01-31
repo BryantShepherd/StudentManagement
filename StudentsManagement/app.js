1599a517aaced1c90f094cb1ba064475c5bcb565
Bryant <laituananh1711@gmail.com>
2020-01-31 08:55:46 +0700

    Add Access-Control-Allow-Headers response header.

--- StudentsManagement/app.js
+++ StudentsManagement/app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const path = require('path');
-const authentication = require('./routes/authenticator');
 const studentManagement = require('./routes/management');
-const registration = require('./routes/registration');
 const app = express();
 
 // Fix issues with CORS error (https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9) 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 
@@ -15,20 +14,14 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// Set view engine
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 // Serve static page
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Authenticate user's login
-app.use('/login', authentication);
-
 app.use('/students', studentManagement);
-
-// app.get('/', (req, res) => {
-//   res.redirect('/students');
-// });
 
 // Establish connection
 const PORT = process.env.port || 5000;
